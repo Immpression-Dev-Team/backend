362c0bd314ede36108f6ed84112c8526865991b6
Mahmoud <tkhattab1999@gmail.com>
2025-03-07 22:01:00 -0500

    ci(admin-users.js): password integration

--- models/admin-users.js
+++ models/admin-users.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const { Schema } = mongoose;
 
@@ -9,29 +10,45 @@ const AdminUserSchema = new Schema(
             unique: true,
             required: [true, "Email is required"],
             match: [
-              /^\w+(\.\w+)*@\w+([\-]?\w+)*(\.\w{2,3})+$/,
-              "Invalid email address",
+                /^\w+(\.\w+)*@\w+([\-]?\w+)*(\.\w{2,3})+$/,
+                "Invalid email address",
             ],
-          },
-          // Define the name field with type String and validation
-          name: {
+        },
+        name: {
             type: String,
             required: [true, "Name is required"],
             minLength: [4, "Name should be at least 4 characters"],
             maxLength: [30, "Name should be less than 30 characters"],
-          },
-          // Define the password field with type String and validation
-          password: {
+        },
+        password: {
             type: String,
             required: [true, "Password is required"],
-            select: false,
+            select: false, // Prevent password from being retrieved in queries
             minLength: [6, "Password should be at least 6 characters"],
             maxLength: [30, "Password should be less than 30 characters"],
-          },
+        },
+        role: {
+            type: String,
+            enum: ["super-admin", "moderator"],
+            required: true,
+        },
     },
     { timestamps: true }
-)
+);
 
-const AdminUserModel = mongoose.models.AdminUserModel || mongoose.model("AdminUser", AdminUserSchema);
+// Middleware to hash password before saving
+AdminUserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-export default AdminUserModel
+// Method to compare passwords
+AdminUserSchema.methods.comparePassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+// Create AdminUser model or retrieve existing one
+const AdminUserModel = mongoose.models.AdminUser || mongoose.model("AdminUser", AdminUserSchema);
+
+export default AdminUserModel;
